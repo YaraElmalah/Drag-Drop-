5bfc34cf95f91be5f6bba1271e638c97b78db3ff
unknown <yaraelmalah2016@gmail.com>
2024-02-26 14:58:20 +0200

    Add AutoBind Decorator

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,49 @@
+//Start Autobind decorator
+const autobind = (_: any, _2: String, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value; 
+    const adjDescriptor: PropertyDescriptor  = {
+        configurable: true, 
+        get() {
+            const bondFn = originalMethod.bind(this); 
+            return bondFn; 
+        }
+
+    }; 
+    return adjDescriptor; 
+}
+
 class Project { 
     templateElement: HTMLTemplateElement; 
     hostingElement: HTMLDivElement; 
     targettingElement: HTMLFormElement; 
+    titleInputElement: HTMLInputElement; 
+    descriptionInputElement: HTMLInputElement; 
+    peopleInputElement: HTMLInputElement; 
     constructor () {
         this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement; 
         this.hostingElement = document.getElementById('app')! as HTMLDivElement; 
         const importedNode = document.importNode(this.templateElement.content, true);
         this.targettingElement = importedNode.firstElementChild as HTMLFormElement; 
+        this.targettingElement.id = 'user-input'; 
+        this.titleInputElement = this.targettingElement.querySelector('#title') as HTMLInputElement; 
+        this.descriptionInputElement = this.targettingElement.querySelector('#description') as HTMLInputElement; 
+        this.peopleInputElement = this.targettingElement.querySelector('#people') as HTMLInputElement; 
         this.attach();  
+        this.configure(); 
     }
     private attach() {
         this.hostingElement.insertAdjacentElement('afterbegin', this.targettingElement); 
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault(); 
+        console.log(this.titleInputElement.value); 
+        console.log(this.descriptionInputElement.value); 
+        console.log(this.peopleInputElement.value); 
+    }
+    private configure() {
+        this.targettingElement.addEventListener('submit', this.submitHandler); 
     }
 }
 
